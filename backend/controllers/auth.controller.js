ce459c0e9a3684e747254af6831e03ecf1cb542c
JAMBITI <biti.jamal@gmail.com>
2024-05-29 13:16:15 +0000

    Define the Login Function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -26,3 +26,41 @@ const loginController = async (request, response) => {
     return response.status(500).json({ error: error.message });
   }
 }
+
+const logoutController = async (request, response) => {
+  return response.status(200).json({ message: "Logged out" });
+}
+
+const forgetPasswordController = async (request, response) => {
+  const { email } = request.body;
+
+  try {
+    const user = await User.findOne({ email})
+    if (!user) {
+      return response.status(404).json({ error: "User not found" });
+    }
+    // generate token
+    const salt = bcrypt.genSaltSync(10);
+    // send email
+    return response.status(200).json({ message: "Email sent", link: `${request.headers.host}/forgetPassword/${salt}` });
+  } catch (error) {
+    return response.status(500).json({ error: error.message });
+  }
+}
+
+const confirmEmailController = async (request, response) => {
+  const { id } = request.params;
+
+  try {
+    const user = await User.findOne({ _id: id });
+    if (!user) {
+      return response.status(404).json({ error: "User not found" });
+    }
+    // update user status
+    const updatedUser = await User.findOneAndUpdate({ _id: user._id }, { status: "active" }, { new: true });
+    return response.status(200).json({ user: updatedUser.status });
+  } catch (error) {
+    return response.status(500).json({ error: error.message });
+  }
+}
+
