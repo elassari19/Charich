9edb49b634e6c8df8eebe2fe1bba16fcbc931efe
JAMBITI <biti.jamal@gmail.com>
2024-05-29 14:36:23 +0000

    Set Up Express Router

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
+
 const {
 	findUserByEmail,
 	findUserByID,
 	createUser,
 	updateUser,
 	deleteUser
+
 } = require("../controllers/user.controller");
 
 const userRouter = express.Router();
@@ -13,10 +15,14 @@ userRouter.post("/", createUser);
 
 userRouter.get("/", findUserByEmail);
 
+
 userRouter.get("/:id", findUserByID);
+
 
 userRouter.patch("/:id", updateUser);
 
+
 userRouter.delete("/:id", deleteUser);
+
 
 module.exports = userRouter;
